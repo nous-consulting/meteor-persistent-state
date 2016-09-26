bca0bd73666d724d4860dd006793c86d6daef2d8
Jaro <jaro@nous.lt>
2016-09-25 22:35:10 -0700

    typo

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'nous:state',
   version: '0.7.0',
-  summary: 'Heroic state for Metoer',
+  summary: 'Heroic state for Meteor',
   git: 'https://github.com/nous-consulting/meteor-persistent-state',
   documentation: 'README.md'
 });
